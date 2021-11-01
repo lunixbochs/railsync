b9a902fb43450701d49cb5b153fb2835c8fcfd11
Ryan Hileman <lunixbochs@gmail.com>
2021-11-01 09:54:24 -0700

    abstract wireguard tunnel into Pipe type

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/lunixbochs/railsync
 go 1.16
 
 require (
+	github.com/pkg/errors v0.9.1
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	golang.zx2c4.com/wireguard v0.0.0-20211028114750-eb6302c7eb71
 )
--- main.go
+++ main.go
@@ -4,49 +4,98 @@ import (
 	"encoding/hex"
 	"fmt"
 	"net"
+	"strconv"
 	"time"
 
+	"github.com/pkg/errors"
 	"golang.zx2c4.com/wireguard/conn"
 	"golang.zx2c4.com/wireguard/device"
 	"golang.zx2c4.com/wireguard/tun/tuntest"
 )
 
+type Pipe struct {
+	privkey device.NoisePrivateKey
+	pubkey  device.NoisePublicKey
+	port    uint16
+	In      <-chan []byte
+	Out     chan<- []byte
+	*device.Device
+}
+
+func NewPipe(mtu int, queuesize int) (*Pipe, error) {
+	tun := NewLoopTun(mtu, queuesize)
+	dev := device.NewDevice(tun, conn.NewDefaultBind(), device.NewLogger(0, ""))
+	privkey := genPrivkey()
+	pubkey := pubkey(&privkey)
+	dev.IpcSet("private_key=" + hex.EncodeToString(privkey[:]))
+	dev.Up()
+
+	config := devconfig(dev)
+	var port int
+	if portstr, ok := config["listen_port"]; !ok {
+		return nil, errors.New("device has no listen_port")
+	} else if porti, err := strconv.Atoi(portstr); err != nil {
+		return nil, errors.Wrap(err, "listen_port invalid")
+	} else {
+		port = porti
+	}
+
+	pipe := &Pipe{
+		privkey: privkey,
+		pubkey:  pubkey,
+		port:    uint16(port),
+		In:      tun.In,
+		Out:     tun.Out,
+		Device:  dev,
+	}
+	return pipe, nil
+}
+
+func (p *Pipe) Port() uint16 {
+	return p.port
+}
+
+func (p *Pipe) Pubkey() device.NoisePublicKey {
+	return p.pubkey
+}
+
+func (p *Pipe) Connect(host string, port uint16, pubkey device.NoisePublicKey) error {
+	ip := net.ParseIP(host)
+	if ip == nil {
+		return errors.New("invalid IP")
+	}
+	p.Device.IpcSet(fmt.Sprintf(`public_key=%s
+endpoint=%s:%d
+allowed_ip=0.0.0.0/0`, hex.EncodeToString(pubkey[:]), ip, port))
+	return nil
+}
+
 func main() {
-	key1, key2 := genPrivkey(), genPrivkey()
-	pubkey1, pubkey2 := pubkey(&key1), pubkey(&key2)
+	pipe1, err := NewPipe(1420, 1024)
+	if err != nil {
+		panic(err)
+	}
+
+	pipe2, err := NewPipe(1420, 1024)
+	if err != nil {
+		panic(err)
+	}
+
+	pipe1.Connect("127.0.0.1", pipe2.Port(), pipe2.Pubkey())
+	pipe2.Connect("127.0.0.1", pipe1.Port(), pipe1.Pubkey())
+
 	localhost := net.ParseIP("127.0.0.1")
-
 	pingPacket := tuntest.Ping(localhost, localhost)
-	tun1 := NewLoopTun(1420, 1024)
-	tun2 := NewLoopTun(1420, 1024)
-
-	dev1 := device.NewDevice(tun1, conn.NewDefaultBind(), device.NewLogger(9999, "dev1 "))
-	dev1.IpcSet("private_key=" + hex.EncodeToString(key1[:]))
-	dev1.Up()
-
-	dev2 := device.NewDevice(tun2, conn.NewDefaultBind(), device.NewLogger(9999, "dev2 "))
-	dev2.IpcSet("private_key=" + hex.EncodeToString(key2[:]))
-	dev2.Up()
-
-	port1 := devconfig(dev1)["listen_port"]
-	port2 := devconfig(dev2)["listen_port"]
-
-	dev1.IpcSet(fmt.Sprintf(`public_key=%s
-endpoint=127.0.0.1:%s
-allowed_ip=0.0.0.0/0`, hex.EncodeToString(pubkey2[:]), port2))
-	dev2.IpcSet(fmt.Sprintf(`public_key=%s
-endpoint=127.0.0.1:%s
-allowed_ip=0.0.0.0/0`, hex.EncodeToString(pubkey1[:]), port1))
 
 	go func() {
-		for packet := range tun2.In {
+		for packet := range pipe2.In {
 			fmt.Println("recv", len(packet))
 		}
 	}()
 
 	for {
 		fmt.Println("send", len(pingPacket))
-		tun1.Out <- pingPacket
+		pipe1.Out <- pingPacket
 		time.Sleep(1 * time.Second)
 	}
 
